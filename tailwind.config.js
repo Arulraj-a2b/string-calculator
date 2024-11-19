0ba9b06506f0ed7f3f6ef24a2ced60e25b6a7922
Arulraj <abofficial1997@gmail.com>
2024-11-19 19:42:51 +0530

    feat add tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        1280: "1280px",
+      },
+      screens: {
+        sm: "768px",
+        md: "1024px",
+        lg: "1280px",
+      },
+      fontFamily: {
+        bold: [`"Roboto-Bold"`],
+        semiBold: [`"Roboto-Medium"`],
+        regular: [`"Roboto-Regular"`],
+      },
+    },
   },
   plugins: [],
 };
